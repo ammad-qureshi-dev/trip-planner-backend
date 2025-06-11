307ecf3b71f377afa6320d2973321bc54dd95e9d
Ammad <ammadq87@my.yorku.ca>
2025-06-11 02:19:03 -0400

    code: creating foundation config for service

---
+++ src/test/java/com/trip_planner/app/TripPlannerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.trip_planner.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class TripPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
